64bb4990f79d83e5b7cacb3d37a58c27dcda4367
David <martirosov.davidka@mail.ru>
2020-09-24 12:10:53 +0300

    iz 2

---
+++ ИЗ 2/8.py
@@ -0,0 +1,35 @@
+def make_buyers_db():
+    data = list()
+    with open('input.txt') as f:
+        for line in f.readlines():
+            data.append(line.rstrip().split())
+
+    return data
+
+
+def make_customers_db():
+    global data
+    data = sorted(make_buyers_db(), key=lambda elem: elem[0])
+    customers = dict()
+    for elem in data:
+        customers[elem[0]] = dict()
+
+    for elem in data:
+        if elem[1] in customers[elem[0]].keys():
+            customers[elem[0]][elem[1]] += int(elem[2])
+        else:
+            customers[elem[0]][elem[1]] = int(elem[2])
+
+    return customers
+
+
+def printer():
+    customers = make_customers_db()
+    with open('output.txt', 'a') as f:
+        for key in customers.keys():
+            f.write(f'{key}:\n')
+            for product in customers.get(key).keys():
+                f.write(f'{product} {customers.get(key).get(product)}\n')
+
+
+printer()
